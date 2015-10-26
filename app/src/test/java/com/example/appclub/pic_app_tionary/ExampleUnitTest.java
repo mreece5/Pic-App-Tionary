06356c964e298a8946a8b220e13f90e37467af70
Brenden Owens <brendenowens@gmail.com>
2015-10-26 16:45:00 -0400

    Beginning
    Initial Commit

---
+++ app/src/test/java/com/example/appclub/pic_app_tionary/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.appclub.pic_app_tionary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
